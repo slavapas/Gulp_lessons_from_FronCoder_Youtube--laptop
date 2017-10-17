05dd1f017f5033e7d73bcf805e9ed3601cc90531
Slava Pascaru <pascaruv@gmail.com>
2017-10-18 00:14:19 +0300

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -26,5 +26,16 @@ gulp.task('stylus', function(){
             title: "style"
         })) //
         .pipe(gp.csso())
+        .pipe(gp.sourcemaps.write())
         .pipe(gulp.dest('build/static/css'));
 });
+
+// add watch gulp.task('<путь тех файлов за которыми будем следить>',gulp.series ('<имя таска который должен исполнятся
+// когда произошли какие-то изменения>')
+//через масив ['....'] можно добавить несколько путей
+gulp.task('watch', function(){
+    gulp.watch('src/pug/**/*.pug',gulp.series('pug')); // после того как в папке c подпапками src/pug были сделаны изминения,
+                                                      // будет вызван таск pug
+    gulp.watch('src/static/**/*.styl',gulp.series('stylus'));
+});
+
